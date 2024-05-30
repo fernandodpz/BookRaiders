f1e9594a77dceb0699012410761e1bc307917084
Gustavo Haag <77116390+gustavorlh96@users.noreply.github.com>
2024-05-30 09:13:41 -0300

    Add first project iteration
    This is a really barebones version of the project, but at least it already runs with no issues on IntelliJ (as it is also pre-configured)

---
+++ src/main/java/br/com/bookraiders/BookraidersApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookraiders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookraidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookraidersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bookraiders/BookraidersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookraiders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookraidersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
